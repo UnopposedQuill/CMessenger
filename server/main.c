6ecea542f0a269cd2109cc5bd2c45d8d680bf203
UnopposedQuill <darklightbeing@gmail.com>
2019-03-06 22:07:49 -0600

    Pruebas iniciales con el socket del servidor

--- server/main.c
+++ server/main.c
@@ -13,12 +13,26 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
 
 /*
  * 
  */
 int main(int argc, char** argv) {
 
+    /*
+     * Primero creo los handles para los sockets que usaré:
+     * server_fd: El socket principal del servidor
+     * new_socket: El socket que intenta comunicarse con el servidor
+     */
+    int server_fd, new_socket, valread;
+    //Primero creo un handler para el socket del servidor
+    if(server_fd = socket(AF_INET, SOCK_STREAM, 0) == 0){//@TODO: Eliminar el == 0
+        //Ocurrió un error al crear el socket
+        perror("Socket creation error");
+        return EXIT_FAILURE;
+        
+    }
     return (EXIT_SUCCESS);
 }
 
