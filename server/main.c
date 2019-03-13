914436825cc08f2040669dd8b03b36d09e32aca1
UnopposedQuill <darklightbeing@gmail.com>
2019-03-12 22:54:28 -0600

    Agregada la funcionalidad al servidor de ser capaz de recibir e imprimir en pantalla mensajes recibidos

--- server/main.c
+++ server/main.c
@@ -14,7 +14,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
+//Me interesa colocar el puerto que usaré como una macro para colocarla alrededor del archivo
+#define PORT 8080
+//También colocar la cantidad de conexiones a la espera
+#define MAX_WAITING_CONNECTIONS 3
+//Finalmente el tamaño del buffer
+#define BUFFER_SIZE 1024
 /*
  * 
  */
@@ -26,13 +34,59 @@ int main(int argc, char** argv) {
      * new_socket: El socket que intenta comunicarse con el servidor
      */
     int server_fd, new_socket, valread;
+    int opt = 1;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
     //Primero creo un handler para el socket del servidor
     if(server_fd = socket(AF_INET, SOCK_STREAM, 0) == 0){//@TODO: Eliminar el == 0
         //Ocurrió un error al crear el socket
         perror("Socket creation error");
         return EXIT_FAILURE;
-        
     }
+    //Creación del socket exitosa, ahora lo que hago es que señalo al SO que reutilice el puerto y la ip
+    if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0){
+        perror("SO couldn't be instructed to reuse the socket address and port");
+        return EXIT_FAILURE;
+    }
+    
+    //En este punto ya es seguro colocar la ip, puertos y protocolos de los que deseo leer
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    
+    //Ahora ya uniré el socket y la dirección, necesito el handler del socket, la dirección
+    //a unir y finalmente la longitud de la dirección
+    if(bind(server_fd, (struct sockaddr*) &address, (socklen_t) addrlen) < 0){
+        perror("Socket couldn't be bound to specified address, port or protocols");
+        return EXIT_FAILURE;
+    }
+    
+    //Ahora falta señalarle al SO que va a escuchar del puerto, con un máximo de conexiones pendientes
+    if(listen(server_fd, MAX_WAITING_CONNECTIONS) < 0){
+        perror("Socket couldn't be signaled to listen");
+        return EXIT_FAILURE;
+    }
+    //Por siempre:
+    while(1){
+        //Intento aceptar una nueva conexión
+        if(new_socket = accept(server_fd, (struct sockaddr*) &address, (socklen_t*) &addrlen) < 0){
+            //No pudo aceptarla
+            perror("Error al aceptar un socket nuevo");
+        }
+        else{
+            //Pudo aceptarla, intento leer los datos
+            if(valread = read(server_fd, buffer, BUFFER_SIZE) == -1){
+                perror("Error al intentar leer los datos");
+            }
+            else{
+                //en este momento valread contiene la cantidad de bytes leídos
+                //Por ahora sólo le ordenaré al servidor imprimir todo lo que lee
+                printf("%s\n", buffer);
+            }
+        }
+    }
+    
     return (EXIT_SUCCESS);
 }
 
